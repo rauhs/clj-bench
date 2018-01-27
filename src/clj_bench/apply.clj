a29a4d6d3353fbea8aa86ee51c5cfd0cc2072506
rauhs <rauhs@users.noreply.github.com>
2018-01-27 18:34:22 +0100

    impl faster applyToHelper gen

--- src/clj_bench/apply.clj
+++ src/clj_bench/apply.clj
@@ -114,9 +114,10 @@
 (defn ret1 [sym]
   (str "Util.ret1(" sym ","sym " = null)"))
 
+;; TODO: RT.seqToArray(
 (defn do-fn-invoke!
   "Generates a "
-  [f ident & [farg & args]]
+  [f ident [farg & args] & [arr-last?]]
   (let [cb (CodeBlock/builder)]
     (.add cb "$L(" (array f))
     (.add cb (ret1 farg) (array))
@@ -127,29 +128,29 @@
     (doseq [arg (butlast args)]
       (.add cb (str ", " (ret1 arg) "\n") (array)))
     (when-some [lar (last args)]
-      (.add cb (str ", " (ret1 lar)) (array)))
+      (.add cb (str ", "
+                    (if arr-last?
+                      (str "RT.seqToArray(" (ret1 lar) ")")
+                      (ret1 lar)))
+            (array)))
     (when (some? args)
       (dotimes [_ ident]
         (.unindent cb)))
     (.build (.add cb ");\n" (array)))))
 
-(defn do-invoke!
-  "Generates a doInvoke"
-  [ident & args]
-  (apply do-fn-invoke! "doInvoke" ident args))
 (defn invoke!
-  [ident & args]
-  (if (some? args)
-    (apply do-fn-invoke! "invoke" ident args)
-    (CodeBlock/of "invoke();\n" (into-array []))))
+  [sym ident args & [arr-last?]]
+  (if (empty? args)
+    (CodeBlock/of (str sym "();\n") (into-array []))
+    (do-fn-invoke! sym ident args arr-last?)))
 
 (defn arg [n] (str "a" n))
 
 ;; NOTE: Can'tt change existings .applyTo arities in IFn (people reify it)
 ;; Add a new interface IApplyMult => .applyToMult with arities:
 ;; ([xs], ([arg0 xs]), ([arg0 arg1 xs]) ...)
-(def atf-iname "IApplyMult")
-(def atf-fname "applyToMult")
+(def atf-iname "IMultiApply")
+(def atf-fname "multiApplyTo")
 
 ;; WORKING CODE FOR RestFn:
 (let [mra "mra"
@@ -164,20 +165,40 @@
           (statement "Object $L = $L.first()" (arg i) args)
           (statement "$L = $L.next()" args args)))
     (>> m "if($L == null)" [args]
-        (add-code "return $L" (apply invoke! 6 (map arg (range 1 (inc i))))))
+        (add-code "return $L" (invoke! "invoke" 6 (map arg (range 1 (inc i))))))
     (>> m "if($L == $L)" [mra i]
         ;; (fn [x & xs]) +
-        (add-code "return $L" (apply do-invoke! 7
-                                     (conj (mapv arg (range 1 (inc i))) args)))))
+        (add-code "return $L" (invoke! "doInvoke" 7 (conj (mapv arg (range 1 (inc i))) args)))))
   (statement m "return throwArity(-1)")
   (code! m))
 
+;; Code for AFn applyToHelper:
+(let [args "args"
+      ifn "ifn"
+      m (-> (method "applyToHelper" #{:public :static})
+            (returns "Object")
+            (add-param "IFn" ifn)
+            (add-param "ISeq" args))]
+  (doseq [i (range 0 21)] ;; 21 for applyTo
+    (when (pos? i)
+      (-> m
+          (statement "Object $L = $L.first()" (arg i) args)
+          (statement "$L = $L.next()" args args)))
+    (>> m "if($L == null)" [args]
+        (add-code "return $L" (invoke! (str ifn ".invoke") 6 (map arg (range 1 (inc i)))))))
+  (add-code m "return $L" (invoke! (str ifn ".invoke") 6
+                                   (conj (mapv arg (range 1 (inc 20))) args) true))
+  (code! m))
+
 ;; Make apply also see AFn, not just RestFn for realistic workload
+;; Always return same type from FNs!
 (comment
-  (let [xs [1 2 3 4 5 6 7] ;(vec (range 4))
+  (let [xs [1 2 3] ;(vec (range 4))
         rfn (fn
-              ([_ _, _ _, _ _ & xs]))
-        afn (fn ([])
+              ([x y z] z)
+              ([x y z & xs] z))
+        afn (fn
+              ([] 1)
               ([x] x)
               ([x y] y)
               ([x y z] z)
